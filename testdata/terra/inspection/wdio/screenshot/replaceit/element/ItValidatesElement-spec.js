63f88aff1ae7fcf93ecda87a4afd6fc70281cacd
Tamas Balog <picimako@protonmail.com>
2023-02-20 13:44:52 +0100

    Update license headers

--- testdata/terra/inspection/wdio/screenshot/replaceit/element/ItValidatesElement-spec.js
+++ testdata/terra/inspection/wdio/screenshot/replaceit/element/ItValidatesElement-spec.js
@@ -1,4 +1,4 @@
-//Copyright 2021 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+//Copyright 2023 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 describe("", () => {
     <caret>Terra.it.validatesElement();
